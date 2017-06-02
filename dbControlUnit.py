54ac05303c3977cb518bf66e0e4405f8f4761292
Ubuntu <ubuntu@ip-172-31-29-171.us-west-1.compute.internal>
2017-06-02 10:29:45 +0000

    Dbwork

---
+++ dbControlUnit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#DB control Unit. The only place I should have to think About DBs is here
+import MySQLdb
+
+class dbCtrl:
+    
+	cursor = db.cursor()
+	
+	#
+	def __init__(self, name, )
+                self.name = name
+				
+	
+	
+	
+	
